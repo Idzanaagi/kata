b25406afa1f5d49bfcc4ea5331883045ebd20652
Idzanaagi <87720620+Idzanaagi@users.noreply.github.com>
2023-11-14 09:05:38 +0300

    Update fundamentals.java

--- fundamentals.java
+++ fundamentals.java
@@ -105,9 +105,22 @@ public class Kata {
 Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
 Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
 */
-
 public class Paper {
   public static int paperWork(int n, int m) {
    return n < 0 || m < 0 ? 0 : n * m;
   }
+}
+
+  // =========================
+  
+  /*
+  Given an integral number, determine if it's a square number:
+  */
+  public class Square {    
+    public static boolean isSquare(int n) {    
+      if (n == 0) {
+        return true;
+      }
+        return n % Math.sqrt(n) == 0;
+    }
 }
